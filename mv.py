a7edb5e83156ee353b1100f7e33b28b9507e1269
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-08-15 18:25:58 -0400

    improve mv.py logging

--- mv.py
+++ mv.py
@@ -2,15 +2,18 @@ import argparse
 from pathlib import Path
 import itertools
 import json
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('source')
 parser.add_argument('destination')
 parser.add_argument('--create', '-c', action='store_true')
+parser.add_argument('--verbose', '-v', action='store_true')
 
 args = parser.parse_args()
 print(f'Moving from {args.source} to {args.destination}')
 
+start = time.time()
 if args.create:
     Path(args.destination).mkdir()
 
@@ -18,7 +21,6 @@ source = list(Path('.').glob(args.source))
 #dest = list(Path('.').glob(args.destination))
 dest = Path(args.destination).expanduser()
 print(f'Found {len(source)} files')
-print(source)
 
 i = 0
 #for i, (a, b) in enumerate(zip(source, dest)):
@@ -35,8 +37,11 @@ for i, a in enumerate(source):
     #print(f'Attempting move from {a} to {b}')
     if not b.exists():
         Path(a).rename(b)
-        print(f'Moved {a} to {b}')
+        if args.verbose: print(f'Moved {a} to {b}')
         session.append(json.dumps({'source': str(a), 'destination': str(b)}))
     #i += 1
+if args.verbose: print('Updating log file')
 with open(Path('~/Desktop/utils/mv_log.json').expanduser(), 'a') as f:
     f.write('\n'.join(session))
+end = time.time()
+if args.verbose: print(f'Finished in {end - start} seconds')
