16cce51a28e6aede4af0f7651f62f66741c21dbe
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-08-15 16:29:03 -0400

    support globbing and path substitutions

--- mv.py
+++ mv.py
@@ -1,14 +1,30 @@
 import argparse
 from pathlib import Path
+import itertools
 
 parser = argparse.ArgumentParser()
 parser.add_argument('source')
 parser.add_argument('destination')
 
 args = parser.parse_args()
-source = Path(args.source)
-dest = Path(args.destination)
+print(f'Moving from {args.source} to {args.destination}')
 
-if dest.is_dir(): dest /= source.name
-Path(source).rename(dest)
-print(f'Moved {source} to {dest}')
+source = list(Path('.').glob(args.source))
+#dest = list(Path('.').glob(args.destination))
+dest = Path(args.destination)
+print(f'Found {len(source)} files')
+
+i = 0
+#for i, (a, b) in enumerate(zip(source, dest)):
+#for a, b in itertools.zip_longest(source, dest):
+for i, a in enumerate(source):
+    b = Path(str(dest))
+    if b.is_dir(): b /= a.name
+    b = Path(str(b)
+        .replace('@', str(a.parent))
+        .replace('%', a.suffix)
+        .replace('#', str(i)))
+    if not b.exists():
+        Path(a).rename(b)
+        print(f'Moved {a} to {b}')
+    #i += 1
